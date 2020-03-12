fc7a70b11e2c9454a6f3bc785a11486d1e29abe7
ws-code <ws.code@qq.com>
2020-03-12 05:56:27 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,17 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    // 注册用户信息
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return ;
+    }
+
     // 个人用户信息界面
     public function show(User $user)
     {
