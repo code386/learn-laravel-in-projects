2b1556de4d371319251ea47da9edf1c7e3da2d5d
ws-code <ws.code@qq.com>
2020-03-13 06:29:42 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -42,4 +42,31 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    // 跳转至用户信息编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // 更新用户信息
+    public function update(User $user, Request $request)
+    {
+        // 验证信息
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        // 判断password是否有值
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        // 执行更新操作
+        $user->update($data);
+        // 提示更新成功
+        session()->flash('success', '个人信息更新成功！');
+        return redirect()->route('users.show', $user);
+    }
+
 }
