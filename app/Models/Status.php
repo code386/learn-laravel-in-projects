da550f4e43cee66f1e1115d6b50499478ed6a224
ws-code <ws.code@qq.com>
2020-04-14 06:11:19 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     protected $table = 'statuses';
+    // 允许更新的字段
+    protected $fillable = ['content'];
 
     // 一对多 用户关联多个文章
     public function user()
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail
 
 Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// 微博删除和编辑
+Route::resource('statuses', 'StatusesController', [ 'only' => ['store', 'destroy']]);
