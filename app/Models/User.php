bbfe7c5485fd4b4fa02e9b75269848dfb556a404
ws-code <ws.code@qq.com>
2020-04-16 13:27:42 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,42 @@ class User extends Authenticatable
 
     protected $table = 'users';
 
+    // 返回真假，判断现在登录的用户A，是否已经关注过用户B
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if ( !is_array($user_ids) ) {
+            $user_ids = compact("user_ids");
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if ( !is_array($user_ids) ) {
+            $user_ids =  compact("user_ids");
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 多对多获得粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 多对多获得关注的人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
     // 查询所有用户发布的微博
     public function feed()
     {
