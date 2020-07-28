edf94c1d70d0b32045e84e11ca54dc8e3a97294f
code386 <ws.code@qq.com>
2020-07-28 20:48:29 +0800

    静态文件浏览器缓存问题并修复footer显示有误问题

--- bbs/laravel/resources/views/layouts/_footer.blade.php
+++ bbs/laravel/resources/views/layouts/_footer.blade.php
@@ -1,8 +1,8 @@
 <!-- 底部导航 -->
 <footer class="footer">
-    <div class="ccontainer">
+    <div class="container">
         <p class="float-left">
-            由<a href="#" target="_blank">code386</a> 设计和编码
+            由 <a href="#" target="_blank">code386</a> 设计和编码
         </p>
 
         <p class="float-right"><a href="mailto:name@emial.com">联系我们</a></p>
